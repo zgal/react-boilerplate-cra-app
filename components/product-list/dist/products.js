42da98490dbae89eae63cda0ffc838682128cf47
Mabel Loh <2269565+zgal@users.noreply.github.com>
2021-05-13 02:36:46 +0800

    import bit react component

---
+++ components/product-list/dist/products.js
@@ -0,0 +1,22 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.products = void 0;
+var products = [{
+  name: 'Phone XL',
+  price: 799,
+  description: 'A large phone with one of the best screens'
+}, {
+  name: 'Phone Mini',
+  price: 699,
+  description: 'A great phone with one of the best cameras'
+}, {
+  name: 'Phone Standard',
+  price: 299,
+  description: ''
+}];
+exports.products = products;
+
+//# sourceMappingURL=products.js.map
